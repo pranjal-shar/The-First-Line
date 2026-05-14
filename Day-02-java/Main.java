d088ff3851cd5cb8ab6f3f23a1708b061a209204
Pranjal Sharma <sharmapranjal0209@gmail.com>
2026-05-14 22:16:37 +0530

    added Day 1 python Array and Day 2 java loops

---
+++ Day-02-java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] goals = {"code", "learn", "grow"};
+
+        for (String goal : goals) {
+            System.out.println(goal);
+        }
+    }
+}
